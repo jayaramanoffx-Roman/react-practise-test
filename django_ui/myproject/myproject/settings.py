b4015ed23613c7d5bf7e6829cc80a89f3cb46fa7
jayaramanoffx-Roman <jayaraman.offx@gmail.com>
2026-06-03 09:18:07 +0530

    host allowed

--- django_ui/myproject/myproject/settings.py
+++ django_ui/myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&09tr0u5+=0yv@5&tk5oauo50n=@h9%f97lf66elmeeu+#y#oc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
